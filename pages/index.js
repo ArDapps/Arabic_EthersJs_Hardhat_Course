3eabd56070cb424ea79e9e6a74eb74c59f745fec
دابس بالعربي <arabicdapps@gmail.com>
2022-10-02 18:02:54 +0200

    add events

--- pages/index.js
+++ pages/index.js
@@ -9,7 +9,7 @@ export default function Home() {
   const[name,setName] = useState("")
   const[symb,setSymb] = useState("")
   const [userBlnc,setUserBlnc] = useState("")
-
+ const [transfers,setTransfers]= useState([])
   const [block,setBlock] = useState("")
 
 
@@ -32,17 +32,23 @@ const fetchUSDT = async()=>{
   setSymb(symbol)
   setUserBlnc( userBalance)
 
-  const latestblock = await provider.getBlockNumber()
-
-  const transcations = await contract.queryFilter("Transfer",latestblock -2,latestblock)
-
-  console.log(transcations[0].address)
-  setBlock(latestblock)
 }
+
+const getTransferData =  ()=>{
+  contract.on("Transfer",(from,to,value,event)=>{
+    let data = {
+      from,to,value:value.toString(),event
+    }
+    setTransfers((oldvalues)=>[...oldvalues,data])
+  
+  });
+
+} 
 
 
   useEffect(()=>{
 fetchUSDT()
+getTransferData()
   },[])
 
 
@@ -59,7 +65,21 @@ fetchUSDT()
 <br/>
 <p className='text-white'>The User of {userAccount} address has Usdt Balance {userBlnc.toString()}</p>
 <br/>
-      <h2 className='btn btn-outline-success'>conatract latest block is {block} </h2>
+{
+  transfers.reverse().map((data,index)=>{
+    return(
+      <div key={index}>
+        <h2>From</h2>
+        <h3 className='text-white'>{data.from}</h3>
+        <h1>To</h1>
+        <h3 className='text-white'>{data.to}</h3>
+        <h1>Value</h1>
+        <h3 className='text-white'>{data.value}</h3>
+      </div>
+    )
+
+  })
+}
 
     </div>
   )
